c7d3fee20aa1a859c3c72ee26d7bbbe61312a73f
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2025-04-03 13:45:23 -0400

    Malicieux

--- electricoon/Scripts/game_manager.gd
+++ electricoon/Scripts/game_manager.gd
@@ -1,6 +1,8 @@
 class_name GameManager
 extends Node
 
+@export var components: Array[Component]
+
 enum SelectionType {
 	DEFAULT
 }
@@ -8,9 +10,21 @@ enum SelectionType {
 var grid_manager: GridManager
 var selection_type: SelectionType = SelectionType.DEFAULT
 
+var current_selected_component: Component
+
 func _ready() -> void:
 	grid_manager = get_tree().get_first_node_in_group("Grid")
 
 func _process(delta: float) -> void:
 	if selection_type == SelectionType.DEFAULT:
 		grid_manager.draw_selection()
+
+func get_component_by_name(name: StringName) -> Component:
+	for component in components:
+		if component.name == name:
+			return component
+	return null
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("Test1"):
+		current_selected_component = get_component_by_name("Wire")

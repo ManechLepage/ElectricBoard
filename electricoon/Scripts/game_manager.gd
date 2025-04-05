0eb5a5b0ac2cb60dcc5e38bb2a73f258f19443d9
Louis Emery <emerylo@educ.college-montreal.qc.ca>
2025-04-05 00:11:56 -0400

    qwe

--- electricoon/Scripts/game_manager.gd
+++ electricoon/Scripts/game_manager.gd
@@ -40,6 +40,4 @@ func _input(event: InputEvent) -> void:
 			erase_component.emit(grid_manager.get_mouse_grid_position())
 		elif current_selected_component:
 			place_component_request.emit(current_selected_component, grid_manager.get_mouse_grid_position())
-	if Input.is_action_just_pressed("RightClick"):
-		erase_component.emit(grid_manager.get_mouse_grid_position())
 	

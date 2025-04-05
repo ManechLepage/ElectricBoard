06955a3185398c1a009b2ee2655a3a5e9c1591f9
Louis Emery <emerylo@educ.college-montreal.qc.ca>
2025-04-05 07:23:43 -0400

    tung tung tung tung tung sahur

--- electricoon/Scenes/main.gd
+++ electricoon/Scenes/main.gd
@@ -7,6 +7,7 @@ extends Node2D
 @onready var hotbar: Node2D = $CanvasLayer/hotbar
 @onready var label: Label = $Label
 @onready var panel: Panel = $Panel
+@onready var short_circuit_sprite: Sprite2D = $Sprite2D
 
 @onready var button: Button = $Button
 @onready var blueprint: TextureRect = $TextureRect
@@ -15,11 +16,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	Game.short_circuit.connect(short_circuit)
 
 func get_components():
 	return components
 
+func short_circuit():
+	short_circuit_sprite.visible = true
+	var tween = create_tween()
+	tween.tween_property(short_circuit_sprite, "modulate", Color.RED, 1 ).set_trans(Tween.TRANS_SINE)
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	label.text = str(Game.money_spent) + " / " + str(budget)

c38f767a996cc02a5b01ada616b8d874959a876c
Louis Emery <emerylo@educ.college-montreal.qc.ca>
2025-04-05 03:21:43 -0400

    sdf

--- electricoon/Scenes/main.gd
+++ electricoon/Scenes/main.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var components: Array[Component]
 @export var budget: int = 0
 @onready var hotbar: Node2D = $CanvasLayer/hotbar
+@onready var label: Label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,4 +14,4 @@ func get_components():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	label.text = str(Game.money_spent) + " / " + str(budget)
